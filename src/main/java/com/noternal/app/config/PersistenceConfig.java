5122128b7d4984b721e168bc2e0a42bca8b2f54f
Michael Hardcastle <maaicle@gmail.com>
2022-02-02 17:25:37 -0700

    updated config for deploy

---
+++ src/main/java/com/noternal/app/config/PersistenceConfig.java
@@ -0,0 +1,20 @@
+package com.noternal.app.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PersistenceConfig {
+    @Bean
+    public DataSource datasource() {
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:postgresql://localhost:5432/noternal");
+        builder.username("newuser");
+        builder.password("password");
+        System.out.println("My custom datasource bean has been initialized and set.");
+        return builder.build();
+    }
+}
